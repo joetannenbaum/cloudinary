4b5b3e2ccff65330b35f46dd5b506b8cd26bdaed
Joe Tannenbaum <joe@joe.codes>
2019-03-27 12:08:37 -0400

    initial commit

---
+++ src/Cloudinary/Laravel/Providers/ServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace JoeTannenbaum\Cloudinary\Laravel\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (config('cloudinary.url')) {
+            \Cloudinary::config_from_url(config('cloudinary.url'));
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('cloudinary', function ($expression) {
+            $parts = collect(explode(',', $expression))->map(function ($part) {
+                return trim($part);
+            });
+
+            $public_id = $parts->shift();
+
+            $params = $parts->implode(',') ?: '[]';
+
+            return "<?php echo cloudinary_url("
+                    . "ltrim("
+                        . "config('cloudinary.base_folder') . '/' . $public_id"
+                    . ", '/'), "
+                    . "array_merge(config('cloudinary.default_params'), $params)); ?>";
+        });
+    }
+}
---
+++ src/Cloudinary/Laravel/config/cloudinary.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'url' => env('CLOUDINARY_URL'),
+    'default_params' => [
+        'secure'       => true,
+        'fetch_format' => 'auto',
+    ],
+    'base_folder' => '',
+];
